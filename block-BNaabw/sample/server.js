824bcaf07c39207ed4f1dc6c3d3bf4b8beb6d794
anup kumar <kanup414@gmail.com>
2022-12-14 23:49:59 +0530

    done

--- block-BNaabw/sample/server.js
+++ block-BNaabw/sample/server.js
@@ -4,3 +4,12 @@ var app = express();
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+
+app.listen(4000, () => {
+    console.log('server is listening on port 4k');
+})
